ac24ae5f951c3ea6ffd2ccf83216ceac04de5c9c
ironmanic-bit <ibagautdinov763@gmail.com>
2026-08-02 18:00:55 +0400

    Enhance Dota Stats with win/loss chart
    Added win/loss statistics and a pie chart for player performance.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import flet as ft
+import flet_charts as fch
 import asyncio
 import requests
-from flags import flag_ru, flag_us, username_field, status_text, progress_bar, back_button, copy_button, search_button
+from flags import title, flag_ru, flag_us, username_field, status_text, progress_bar, back_button, copy_button, search_button
 from localization import t, set_lang
 
 def main(page: ft.Page):
@@ -12,12 +13,15 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     search_button.on_click = lambda e: page.run_task(handle_search, username_field.value, status_text, progress_bar, search_button, title, username_field, t)
+    
 
     def update_ui():
         username_field.label = t('username_field')
         title.value = t('title')
         search_button.content= t('search_button')
         page.update()
+
+    update_ui()
     
 
     def translate_us(e):     
@@ -134,8 +138,31 @@ def main(page: ft.Page):
 
         account_id = search_data[0]['account_id']
 
+
         matches_response = requests.get(f"https://api.opendota.com/api/players/{account_id}/matches")
+        wl = requests.get(f"https://api.opendota.com/api/players/{account_id}/wl") # Парсинг матчей, а также K/D статистику.
+        wl_stats = wl.json()
+
+
         matches_data = matches_response.json()
+        try:
+            last_match = matches_data[0]
+        except IndexError as e:
+            status_text.value = t("status_error_NFG_2")
+            progress_bar.visible = False
+            page.update()
+            await asyncio.sleep(8)
+            status_text.value = ""
+            page.update()
+            return
+
+        
+        heroes_response = requests.get(f"https://api.opendota.com/api/heroes")
+        all_heroes = heroes_response.json()
+        hero_name_map = {hero['id']: hero['localized_name'] for hero in all_heroes}
+
+        hero_id = last_match['hero_id']
+        last_hero = hero_name_map[hero_id]
 
         if not matches_data:
             status_text.value = t("status_error_not_found_matches")
@@ -149,9 +176,9 @@ def main(page: ft.Page):
         await outro(search_button, title, username_field, status_text, progress_bar)
         status_text.value = ""
         progress_bar.visible = False 
-        show_stats(account_id, matches_data, search_data)
+        show_stats(account_id, matches_data, search_data, last_hero, wl_stats)
 
-    def show_stats(account_id, matches_data, search_data):
+    def show_stats(account_id, matches_data, search_data, last_hero, wl_stats):
         clear()
 
 
@@ -167,8 +194,31 @@ def main(page: ft.Page):
         )
 
         matches_count = len(matches_data)
+
+        win = wl_stats["win"]
+        lose = wl_stats["lose"]
         
-        txt_info = t('info').format(account_id=account_id, matches_data=matches_count)
+
+        chart = fch.PieChart(
+            sections=[
+                fch.PieChartSection(
+                    value=win if win > 0 else 1,
+                    title=f"{round(win/(win+lose) * 100)}%",
+                    color=ft.Colors.BLUE,
+                    radius=80,
+                ),
+                fch.PieChartSection(
+                    value=lose if lose > 0 else 1,
+                    title=f"{round(lose/(win+lose) * 100)}%",
+                    color=ft.Colors.RED,
+                    radius=80,
+                )
+            ],
+            sections_space = 1,
+            width=160,
+            height=160,
+        )
+        txt_info = t('info').format(account_id=account_id, matches_data=matches_count, last_hero=last_hero)
 
         info = ft.Text(
             f"{txt_info}",
@@ -194,10 +244,12 @@ def main(page: ft.Page):
                                     controls=[
                                         name_text,
                                         ft.Container(height=15),
-                                        info     
+                                        info,
+                                        ft.Container(height=10),
+                                        chart
                                     ],
                                     alignment=ft.MainAxisAlignment.CENTER,
-                                    spacing=2
+                                    spacing=1
                                 ),
                                 ft.Container(width=10),
                                 copy_button
